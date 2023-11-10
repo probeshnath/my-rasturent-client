ad4a30b18781294d3d12ae9acb96fa47135a9075
probeshnath <probeshn@gmail.com>
2023-11-10 14:20:54 +0600

    featured add

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,8 @@
 import Banner from './Banner'
 import Category from './Category'
+import Featured from './Featured'
 import PopularMenu from './PopularMenu'
+import Testimonials from './Testimonials'
 
 const Home = () => {
  
@@ -9,6 +11,8 @@ const Home = () => {
         <Banner />
         <Category />
         <PopularMenu />
+        <Featured />
+        <Testimonials />
     </div>
   )
 }
