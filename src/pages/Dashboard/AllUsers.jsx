b1b4ad5df9380b049e4b767f7bcdc95d81c1aea8
probeshnath <probeshn@gmail.com>
2023-11-19 00:10:52 +0600

    delete and make admin user01~

--- src/pages/Dashboard/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers.jsx
@@ -1,16 +1,59 @@
 import { useQuery } from '@tanstack/react-query'
 import React from 'react'
 import useAxiosSecure from '../../hooks/useAxiosSecure'
+import { MdDelete } from 'react-icons/md'
+import { FaUser } from 'react-icons/fa'
+import Swal from 'sweetalert2'
 
 const AllUsers = () => {
     const axiosSecure = useAxiosSecure()
-    const { data: users = [] } = useQuery({
+    const { data: users = [],refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
             const res = await axiosSecure.get("/users");
             return res.data;
         }
     })
+
+    // admin user
+    const handleMakeAdmin = (user) =>{
+        // 
+        console.log('"admin user')
+    }
+
+
+    // handle delete user
+    const handleDeleteUser = (user) =>{
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+
+                axiosSecure.delete(`/users/${user._id}`)
+                    .then(res => {
+                        // console.log(res.data)
+                        if (res.data.deletedCount > 0) {
+                            refetch()
+                              Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                              });
+                        }
+                    })
+                    .catch(error => {
+                        console.log(error)
+                    })
+            }
+        });
+    }
     return (
         <div>
             <div className='flex justify-evenly my-4'>
@@ -22,20 +65,27 @@ const AllUsers = () => {
                     {/* head */}
                     <thead>
                         <tr>
-                            <th></th>
+                            <th>#</th>
                             <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                            <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
-                            users.map((user,inx) => (
+                            users.map((user, inx) => (
                                 <tr key={user._id}>
                                     <th>{inx + 1}</th>
                                     <td>{user.name}</td>
                                     <td>{user.email}</td>
-                                    <td>Blue</td>
+                                    <td>
+                                        <button onClick={() => handleMakeAdmin(user)} className="btn"><FaUser className='text-blue-600 text-xl' /></button>
+                                     </td>
+
+                                    <td>
+                                        <button onClick={() => handleDeleteUser(user)} className="btn"><MdDelete className='text-red-600 text-xl' /></button>
+                                     </td>
                                 </tr>
                             ))
                         }
